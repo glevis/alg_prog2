cc26f50c2cec732aaf413281a8bbcadadbe32d48
Gavin Levis <levis.gavin@gmail.com>
2023-03-29 11:13:25 -0500

    missed changes from previous commit

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
-from util.util import QueryParser
+from util.util import QueryParser, StoreParser
+from util.haversine import haversine
 
 def main():
     qp = QueryParser()
+    wb = StoreParser()
+    sb = StoreParser()
     qp.parse("tests/Queries.csv")
+    wb.parse("tests/whataburger/WhataburgerData.csv")
+    sb.parse("tests/starbucks/StarbucksData.csv")
+    
     for q in qp.queries:
-        print(q.point.x, q.point.y)
+        print(f"The {q.stores} closest Whataburgers to ({q.point.x}, {q.point.y})")
+        distances = []
+        for store in wb.stores:
+            distance = haversine(q.point.x, q.point.y, store.lat, store.long)
+            distances.append(distance)
+            # compute order statistics
+
+            # sort first 30 stores in increasing order (any O(n^2))
+            
+            # output stores from closest to farthest
+            # format: [store] #[id]. [addr] [street], [city], [state], [zip]. - [distance] miles
+
+    for q in qp.queries:
+        print(f"The {q.stores} closest Starbucks to ({q.point.x}, {q.point.y})")
 
 if __name__ == "__main__":
     main()
---
+++ util/util.py
@@ -0,0 +1,50 @@
+import csv 
+
+class QueryParser:
+    def __init__(self):
+        self.queries = []
+
+    def parse(self, path):
+        f = open(path, "r")
+        # header
+        f.readline() 
+        for line in f:
+           line = line.replace('\n', '')
+           x, y, stores = line.split(',') 
+           point = Point(x, y)
+           query = Query(point, stores)
+           self.queries.append(query)
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Query:
+    def __init__(self, point, stores):
+        self.point = point
+        self.stores = stores
+
+class StoreParser:
+    def __init__(self):
+        self.stores = []
+
+    def parse(self, path):
+        with open(path, newline='') as f:
+            # header
+            next(f)
+            lines = csv.reader(f, delimiter=',', quotechar='"')
+            for line in lines:
+                id, addr, city, state, zip, lat, long = line
+                store = Store(id, addr, city, state, zip, lat, long)
+                self.stores.append(store)
+
+class Store:
+    def __init__(self, id, addr, city, state, zip, lat, long):
+        self.id = id
+        self.addr = addr
+        self.city = city
+        self.state = state
+        self.zip = zip
+        self.lat = lat
+        self.long = long
