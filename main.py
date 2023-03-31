0e840baee74ee298dbbd8056d786013d37220da0
TMJukebox <adenmrojas@gmail.com>
2023-03-30 20:09:34 -0500

    added compute logic
    importing math
    added the compute logic from the video provided in the assign doc  https://www.youtube.com/watch?v=Camxl1fTzaw&t=290s

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 from util.util import QueryParser, StoreParser
 from util.haversine import haversine
 
@@ -16,7 +17,20 @@ def main():
             distance = haversine(q.point.x, q.point.y, store.lat, store.long)
             distances.append(distance)
             # compute order statistics
-
+            
+            radiusOfEarth = '3958.8'# miles
+            
+            #convert lat/long to radians
+            lat1 = math.radians(q.point.x)
+            lon1 = math.radians(q.point.y)
+            lat2 = math.radians(store.lat)
+            lon2 = math.radians(store.long)
+            
+            #then apply harvesine formula
+            a = math.sin((lat2 - lat1)/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin((lon2 - lon1)/2)**2
+            c = 2 * math.atan2(math.sqrt(a), math.sqrt(1-a))
+            distance = radiusOfEarth * c
+            
             # sort first 30 stores in increasing order (any O(n^2))
             
             # output stores from closest to farthest
